fd9393dfe120d09a3809046bbcda205234889da5
Tom Emming <tom@tinux-it.nl>
2025-06-29 16:29:37 +0200

    Chore; adds small footer

--- resources/views/livewire/driver-schedule.blade.php
+++ resources/views/livewire/driver-schedule.blade.php
@@ -253,11 +253,7 @@
 
         <!-- Footer Message -->
         <div class="mt-12 text-center">
-            <div class="inline-flex items-center justify-center space-x-2 text-gray-600">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
-                </svg>
-            </div>
+            <div class="text-gray-500 text-xs mt-4"><a href="https://tomemming.nl">Made with ❤️ by Tom Emming</a></div>
         </div>
 
     </div>
